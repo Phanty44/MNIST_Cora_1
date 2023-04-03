9669c6b1bf90cf9a20e2c83fddb1f84f4a595878
Krzysztof Dwuznik <249946@edu.p.lodz.pl>
2023-04-03 23:15:38 +0200

    Initial models

--- Cora.py
+++ Cora.py
@@ -1,13 +1,62 @@
-import sklearn
 import torch
 import torchvision
+import networkx as nx
+import numpy as np
+import pandas as pd
 import tabulate
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.neural_network import MLPClassifier
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score, classification_report
 from torch_geometric.datasets import Planetoid
-from torch_geometric.loader import DataLoader
+from torch_geometric.utils.convert import to_networkx
+from torch_geometric.data import data, dataset, in_memory_dataset
 
-cora = Planetoid(".", "Cora")
-data = cora[0]
-print(data.x)
-print(len(data.x[0]))
-print(data.y)
-print(len(data.y))
+cora = Planetoid(".", "Cora", "public")
+coraData = cora[0]
+vectors = coraData.x
+labels = coraData.y
+
+train_x = coraData.x[:640].numpy()
+train_y = coraData.y[:640].numpy()
+train_edges = coraData.edge_index[:640].numpy()
+
+test_x = coraData.x[-1000:].numpy()
+test_y = coraData.y[-1000:].numpy()
+test_edges = coraData.edge_index[-1000:].numpy()
+
+# #K-nn
+# for k in [1, 3, 5, 7, 9, 11]:
+#     knn = KNeighborsClassifier(k)
+#     knn.fit(train_x, train_y)
+#
+#     knn_pred = knn.predict(test_x)
+#     print(classification_report(test_y, knn_pred))
+
+# #MLP
+# layers = 50
+# activation = "relu"
+# solver = "sgd"
+# alpha = 0.0001
+# learning_rate = 0.1
+# max_iter = 200
+#
+# mlp = MLPClassifier(
+#     hidden_layer_sizes=layers,
+#     activation=activation,
+#     solver=solver,
+#     alpha=alpha,
+#     learning_rate_init=learning_rate,
+#     max_iter=max_iter)
+#
+# mlp.fit(train_x, train_y)
+# mlp_pred = mlp.predict(test_x)
+# print(classification_report(test_y, mlp_pred))
+
+
+# SVM
+svm = SVC(kernel="linear", gamma="auto")
+
+svm.fit(train_x, train_y)
+svm_pred = svm.predict(test_x)
+print(classification_report(test_y, svm_pred))
