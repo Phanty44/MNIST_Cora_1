d5f493b9483593360b7d6d2d618281152bf2083d
Phanty <saitek123@op.pl>
2023-04-04 17:56:55 +0200

    Added torchvision dataset

--- main.py
+++ main.py
@@ -1,18 +1,27 @@
-from sklearn import datasets
-
-import visualization
-import feature_extr
-import MNIST_KNN, MNIST_MLP
-
+import numpy as np
 from sklearn.model_selection import train_test_split
+from torchvision import datasets as datas
 
-x, y = datasets.fetch_openml('mnist_784', version=1, return_X_y=True, parser='auto')
-x = x / 255.0
-x = x.to_numpy()
-y = y.to_numpy()
+import MNIST_KNN
+import MNIST_MLP
+import feature_extr
+import visualization
 
-(x_train, x_test, y_train, y_test) = train_test_split(x, y, test_size=10000, random_state=84, shuffle=True)
-# uniform filter, pooling
+x_train = []
+x_test = []
+y_train = []
+y_test = []
+
+trainset = datas.MNIST('~/.pytorch/MNIST_data/', download=True, train=True)
+x_train = np.array(trainset.data)
+x_train = np.array(x_train, dtype="float") / 255
+y_train = np.array(trainset.targets)
+
+testset = datas.MNIST('~/.pytorch/MNIST_data/', download=True, train=False)
+x_test = np.array(testset.data)
+x_test = np.array(x_test, dtype="float") / 255
+y_test = np.array(testset.targets)
+
 (x_train, x_val, y_train, y_val) = train_test_split(x_train, y_train, test_size=0.1, random_state=84)
 # Extract HOG features from the training data
 
@@ -24,7 +33,7 @@ kVals = range(1, 10, 1)
 hog_features_train, hog_features_test, hog_features_eval = feature_extr.hog_features_extr(x_train, x_test, x_val)
 x_train_pooled, x_test_pooled, x_val_pooled = feature_extr.mp_feature_extr(x_train, x_test, x_val)
 
-# MNIST_KNN.knn_hog(hog_features_train, y_train, hog_features_eval, y_val, hog_features_test, y_test, kVals)
-# MNIST_KNN.knn_mp(x_train_pooled, y_train, x_val_pooled, y_val, x_test_pooled, y_test, kVals)
+MNIST_KNN.knn_hog(hog_features_train, y_train, hog_features_eval, y_val, hog_features_test, y_test, kVals)
+MNIST_KNN.knn_mp(x_train_pooled, y_train, x_val_pooled, y_val, x_test_pooled, y_test, kVals)
 MNIST_MLP.mlp(hog_features_train, y_train, hog_features_eval, y_val, hog_features_test, y_test)
 MNIST_MLP.mlp(x_train_pooled, y_train, x_val_pooled, y_val, x_test_pooled, y_test)
