fb51fa47f13227384914c5722d32f27afb91e658
Krzysztof Dwuznik <249946@edu.p.lodz.pl>
2023-04-05 13:18:43 +0200

    Extraction from edges

--- Cora.py
+++ Cora.py
@@ -25,33 +25,33 @@ test_x = coraData.x[-1000:].numpy()
 test_y = coraData.y[-1000:].numpy()
 test_edges = coraData.edge_index[-1000:].numpy()
 
-# #K-nn
+#K-nn
 # for k in [1, 3, 5, 7, 9, 11]:
-#     knn = KNeighborsClassifier(k)
-#     knn.fit(train_x, train_y)
-#
-#     knn_pred = knn.predict(test_x)
-#     print(classification_report(test_y, knn_pred))
+knn = KNeighborsClassifier(11)
+knn.fit(train_x, train_y)
 
-# #MLP
-# layers = 50
-# activation = "relu"
-# solver = "sgd"
-# alpha = 0.0001
-# learning_rate = 0.1
-# max_iter = 200
-#
-# mlp = MLPClassifier(
-#     hidden_layer_sizes=layers,
-#     activation=activation,
-#     solver=solver,
-#     alpha=alpha,
-#     learning_rate_init=learning_rate,
-#     max_iter=max_iter)
-#
-# mlp.fit(train_x, train_y)
-# mlp_pred = mlp.predict(test_x)
-# print(classification_report(test_y, mlp_pred))
+knn_pred = knn.predict(test_x)
+print(classification_report(test_y, knn_pred))
+
+#MLP
+layers = 50
+activation = "relu"
+solver = "sgd"
+alpha = 0.0001
+learning_rate = 0.1
+max_iter = 200
+
+mlp = MLPClassifier(
+    hidden_layer_sizes=layers,
+    activation=activation,
+    solver=solver,
+    alpha=alpha,
+    learning_rate_init=learning_rate,
+    max_iter=max_iter)
+
+mlp.fit(train_x, train_y)
+mlp_pred = mlp.predict(test_x)
+print(classification_report(test_y, mlp_pred))
 
 
 # SVM
@@ -59,4 +59,53 @@ svm = SVC(kernel="linear", gamma="auto")
 
 svm.fit(train_x, train_y)
 svm_pred = svm.predict(test_x)
+print(classification_report(test_y, svm_pred))
+
+edges = [[0 for col in range(2708)] for row in range(2708)]
+
+# extract edges
+for i in range(len(coraData.edge_index[0])):
+    edges[coraData.edge_index[0][i]][coraData.edge_index[1][i]] = 1
+
+new_x = []
+for i in range(len(coraData.x)):
+    new_x.append(np.append(coraData.x[i], edges[i]))
+
+new_train_x = new_x[:640]
+new_test_x = new_x[-1000:]
+
+#K-nn
+# for k in [1, 3, 5, 7, 9, 11]:
+knn = KNeighborsClassifier(11)
+knn.fit(new_train_x, train_y)
+
+knn_pred = knn.predict(new_test_x)
+print(classification_report(test_y, knn_pred))
+
+#MLP
+layers = 50
+activation = "relu"
+solver = "sgd"
+alpha = 0.0001
+learning_rate = 0.1
+max_iter = 200
+
+mlp = MLPClassifier(
+    hidden_layer_sizes=layers,
+    activation=activation,
+    solver=solver,
+    alpha=alpha,
+    learning_rate_init=learning_rate,
+    max_iter=max_iter)
+
+mlp.fit(new_train_x, train_y)
+mlp_pred = mlp.predict(new_test_x)
+print(classification_report(test_y, mlp_pred))
+
+
+# SVM
+svm = SVC(kernel="linear", gamma="auto")
+
+svm.fit(new_train_x, train_y)
+svm_pred = svm.predict(new_test_x)
 print(classification_report(test_y, svm_pred))
